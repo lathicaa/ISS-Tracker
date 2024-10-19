f45341621124395110e2cb4e2ded1ee89b945f67
Lathica Arunkumar <larunkum@uwaterloo.ca>
2024-10-19 12:13:08 -0400

    successful changes to latitude longitude

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ iss_longitude = convert_nasa["iss_position"]["longitude"]
 iss_latitude = float(iss_latitude)
 iss_longitude = float(iss_longitude)
 
-#43.505085, -80.702434
-# conditional to check if ISS above Toronto range
-if abs(iss_latitude - 43.6532) <= abs(43.4730 - 43.6532) and abs(iss_longitude - 79.3832 ) <= abs(80.5395 - 79.3832):
-    token = os.getenv("AUTH_TOKEN")
+#40.7128° N, 74.0060° W NEW YORK
+#43.6532° N, 79.3832° W TORONTO
 
+# conditional to check if ISS above Toronto range
+if abs(iss_latitude - 43.6532) <= abs(43.4730 - 40.7128) and abs(iss_longitude - 79.3832) <= abs(79.3832 - 74.0060):
     # Send SMS message to individual phone numbers
     account_sid = os.getenv("ACCOUNT_SID")
     auth_token = os.getenv("AUTH_TOKEN")
@@ -42,4 +42,83 @@ if abs(iss_latitude - 43.6532) <= abs(43.4730 - 43.6532) and abs(iss_longitude -
         body=f'The ISS is currently above you! Latitude: {iss_latitude}°, Longitude: {iss_longitude}° ',
         to=os.getenv("ADITYA_NUMBER")
     )
+
+    #message = client.messages.create(
+        #from_='+6475511906',
+        #body=f'The ISS is currently above you! Latitude: {iss_latitude}°, Longitude: {iss_longitude}° ',
+        #to=os.getenv("KRISH_NUMBER")
+    #)
+
+    #message = client.messages.create(
+        #from_='+',
+        #body=f'The ISS is currently above you! Latitude: {iss_latitude}°, Longitude: {iss_longitude}° ',
+        #to=os.getenv("NAME_NUMBER")
+    #)
+
+#49.8954° N, 97.1385° W WINNIPEG
+#44.9778° N, 93.2650° W MINNEAPOLIS
+elif abs(iss_latitude - 49.8954) <= abs(49.8954 - 44.9778) and abs(iss_longitude - 97.1385) <= abs(97.1385 - 93.2650):
+
+    # Send SMS message to individual phone numbers
+    account_sid = os.getenv("ACCOUNT_SID")
+    auth_token = os.getenv("AUTH_TOKEN")
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_='+18646190794',
+        body=f'The ISS is approaching you in the next ten minutes! Latitude: {iss_latitude}°, Longitude: {iss_longitude}° ',
+        to=os.getenv("LATHICA_NUMBER")
+    )
+
+    message = client.messages.create(
+        from_='+18646190794',
+        body=f'The ISS is appraoching you in the next ten minutesr! Latitude: {iss_latitude}°, Longitude: {iss_longitude}° ',
+        to=os.getenv("ADITYA_NUMBER")
+    )
+
+    #message = client.messages.create(
+        #from_='+6475511906',
+        #body=f'The ISS is approaching you in the next ten minutes! Latitude: {iss_latitude}°, Longitude: {iss_longitude}° ',
+        #to=os.getenv("KRISH_NUMBER")
+    #)
+
+    #message = client.messages.create(
+        #from_='+',
+        #body=f'The ISS is approaching you in the next ten minutes! Latitude: {iss_latitude}°, Longitude: {iss_longitude}° ',
+        #to=os.getenv("NAME_NUMBER")
+    #)
+
+#49.2827° N, 123.1207° W VANCOUVER
+#51.0447° N, 114.0719° W CALGARY
+elif abs(iss_latitude - 49.2827) <= abs(49.2827 - 51.0447) and abs(iss_longitude - 123.1207) <= abs(123.1207 - 114.0719):
+
+    # Send SMS message to individual phone numbers
+    account_sid = os.getenv("ACCOUNT_SID")
+    auth_token = os.getenv("AUTH_TOKEN")
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_='+18646190794',
+        body=f'The ISS is approaching you in the next half hour! Latitude: {iss_latitude}°, Longitude: {iss_longitude}° ',
+        to=os.getenv("LATHICA_NUMBER")
+    )
+
+    message = client.messages.create(
+        from_='+18646190794',
+        body=f'The ISS is appraoching you in the next half hour! Latitude: {iss_latitude}°, Longitude: {iss_longitude}° ',
+        to=os.getenv("ADITYA_NUMBER")
+    )
+
+    #message = client.messages.create(
+        #from_='+6475511906',
+        #body=f'The ISS is approaching you in the next half hour! Latitude: {iss_latitude}°, Longitude: {iss_longitude}° ',
+        #to=os.getenv("KRISH_NUMBER")
+    #)
+
+    #message = client.messages.create(
+        #from_='+',
+        #body=f'The ISS is approaching you in the next half hour! Latitude: {iss_latitude}°, Longitude: {iss_longitude}° ',
+        #to=os.getenv("NAME_NUMBER")
+    #)
+
 
